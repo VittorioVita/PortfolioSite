9f844b426fc5686a0a7d5766d2b3c82ce758cd73
vittorio caporale <vittoriocaporalevita@gmail.com>
2025-11-29 08:01:27 +0100

    ttry

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client' // <--- QUESTO MANCAVA!
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from './components/ScrollToTop' // <--- IMPORTALO QUI
+import { HashRouter } from 'react-router-dom' // Usiamo solo HashRouter per GitHub Pages
+import ScrollToTop from './components/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ScrollToTop /> {/* <--- AGGIUNGILO QUI, PRIMA DI APP */}
+    <HashRouter>
+      <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
